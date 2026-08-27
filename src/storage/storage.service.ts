e2c03a9944be5cf778ced425172ea6c9075a42bb
Pablo George <pablogeokar@hotmail.com>
2026-08-27 10:30:49 -0300

    melhorias no path de salvamento dos comprovantes

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -87,11 +87,11 @@ export class StorageService {
   }
 
   receiptObjectKey(input: {
-    obligationId: string;
+    cnpjCpf: string;
     receiptId: string;
     extension: string;
   }) {
-    return `comprovantes/${input.obligationId}/${input.receiptId}.${input.extension}`;
+    return `clientes/${input.cnpjCpf}/comprovantes/${input.receiptId}.${input.extension}`;
   }
 
   logoObjectKey(input: { cnpjCpf: string; extension: string }) {
